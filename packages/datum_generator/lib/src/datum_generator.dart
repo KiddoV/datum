9fdba6a89d87261cf9b1232336f09bd284a44634
Viet Ho <viet.ho.03@gmail.com>
2026-03-04 10:22:38 -0600

    initial fix

--- packages/datum_generator/lib/src/datum_generator.dart
+++ packages/datum_generator/lib/src/datum_generator.dart
@@ -236,7 +236,7 @@ class DatumGenerator extends GeneratorForAnnotation<DatumSerializable> {
             '\n  /// Get the related ${isList ? 'entities' : 'entity'}',
           );
           buffer.writeln(
-            '  $type get $publicName => (this as $className).$fieldName;',
+            '  $type get $publicName => relations[\'$publicName\']?.value as $type;',
           );
 
           buffer.writeln(
@@ -245,7 +245,7 @@ class DatumGenerator extends GeneratorForAnnotation<DatumSerializable> {
           buffer.writeln('  set $publicName($type value) {');
           buffer.writeln('    if (this is $className) {');
           buffer.writeln(
-            '      (this as $className).datumRelations[\'$publicName\']?.setRaw(value);',
+            '      relations[\'$publicName\']?.setRaw(value);',
           );
           buffer.writeln('    }');
           buffer.writeln('  }');
@@ -255,9 +255,12 @@ class DatumGenerator extends GeneratorForAnnotation<DatumSerializable> {
 
     // relations override for RelationalDatumEntity
     if (isRelational) {
+      buffer.writeln('\n  late final Map<String, Relation> _cachedRelations =');
+      buffer.writeln('      (this as $className).datumRelations;');
+
       buffer.writeln('\n  @override');
       buffer.writeln(
-        '  Map<String, Relation> get relations => (this as $className).datumRelations;',
+        '  Map<String, Relation> get relations => _cachedRelations;',
       );
     }
 
